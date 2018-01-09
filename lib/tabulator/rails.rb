d619445351833645597c198ef28fcac39c100d87
tanvir hasan <tanvir002700@gmail.com>
2018-01-09 19:40:37 +0600

    init project

---
+++ lib/tabulator/rails.rb
@@ -0,0 +1,7 @@
+require "tabulator/rails/version"
+
+module Tabulator
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/tabulator/rails/version.rb
@@ -0,0 +1,5 @@
+module Tabulator
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
